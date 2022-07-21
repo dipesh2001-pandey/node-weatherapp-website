a11e4e30000d6598115854020f24492d75f9cc16
Dipesh Pandey <dkpandey@localhost.localdomain>
2022-07-21 07:01:10 +0530

    push app to heroku

--- public/js/app_a.js
+++ public/js/app_a.js
@@ -17,34 +17,32 @@ weatherform.addEventListener("submit", (e) => {
   e.preventDefault();
   disp.textContent = ".....Loading";
   const location = search.value;
-  fetch(`http://localhost:3000/weather?address=${location}`).then(
-    (response) => {
-      response.json(response).then((data) => {
-        if (data.error) {
-          disp.textContent = data.error;
-          place.textContent = `Place:`;
-          weatherdescp.innerHTML = ` <br/> Feels like °C`;
-          temperature.textContent = ` °C`;
-          windsped.textContent = ` Km/h`;
-          winddirn.textContent = ``;
-          humidity.textContent = ` %`;
-          uv.textContent = `UV:`;
-          time.textContent = `Time: `;
-          weather_image.src = `https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-weather/ilu1.webp`;
-        } else {
-          disp.textContent = "";
+  fetch(`/weather?address=${location}`).then((response) => {
+    response.json(response).then((data) => {
+      if (data.error) {
+        disp.textContent = data.error;
+        place.textContent = `Place:`;
+        weatherdescp.innerHTML = ` <br/> Feels like °C`;
+        temperature.textContent = ` °C`;
+        windsped.textContent = ` Km/h`;
+        winddirn.textContent = ``;
+        humidity.textContent = ` %`;
+        uv.textContent = `UV:`;
+        time.textContent = `Time: `;
+        weather_image.src = `https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-weather/ilu1.webp`;
+      } else {
+        disp.textContent = "";
 
-          place.textContent = `Place: ${data.location}`;
-          weatherdescp.innerHTML = `${data.forecast.weather_description} <br/> Feels like ${data.forecast.feelslike}°C`;
-          temperature.textContent = `${data.forecast.temperature} °C`;
-          windsped.textContent = `${data.forecast.wind_speed} Km/h`;
-          winddirn.textContent = `${data.forecast.wind_dirn}`;
-          humidity.textContent = `${data.forecast.humidity}%`;
-          uv.textContent = `UV: ${data.forecast.uv_index}`;
-          time.textContent = `Time: ${data.forecast.observation_time}`;
-          weather_image.src = `${data.forecast.weather_icons[0]}`;
-        }
-      });
-    }
-  );
+        place.textContent = `Place: ${data.location}`;
+        weatherdescp.innerHTML = `${data.forecast.weather_description} <br/> Feels like ${data.forecast.feelslike}°C`;
+        temperature.textContent = `${data.forecast.temperature} °C`;
+        windsped.textContent = `${data.forecast.wind_speed} Km/h`;
+        winddirn.textContent = `${data.forecast.wind_dirn}`;
+        humidity.textContent = `${data.forecast.humidity}%`;
+        uv.textContent = `UV: ${data.forecast.uv_index}`;
+        time.textContent = `Time: ${data.forecast.observation_time}`;
+        weather_image.src = `${data.forecast.weather_icons[0]}`;
+      }
+    });
+  });
 });
--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const hbs = require("hbs");
 const utils = require("./utils/utils");
 
 const app = express();
+const port = process.env.PORT || 3000;
 ///paths exsist here///
 const publicdirPath = path.join(__dirname, "../public");
 const viewspath = path.join(__dirname, "../templates/views");
@@ -67,6 +68,6 @@ app.get("*", (req, res) => {
     errormsg: "Page Not found",
   });
 });
-app.listen(3000, () => {
-  console.log("Server is up onport 3000");
+app.listen(port, () => {
+  console.log(`Server is up onport ${port}`);
 });
